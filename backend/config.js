61f9823fe9f99b94ca8b41457d63dd4dfb424047
Athul Vijayan <147768974+athul-vijayan@users.noreply.github.com>
2023-11-20 19:34:30 +0530

    Delete backend

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-export const mongodbURL='mongodb+srv://username:password5@bookproject.vhjyxgr.mongodb.net/?retryWrites=true&w=majority'
-// export const mongodbURL='mongodb+srv://myusername:mypassword@cluster0.dybgada.mongodb.net/?retryWrites=true&w=majority'
-// export const mongoDBURL ='mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
